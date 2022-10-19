b7101396fe02faf195e8558adea31ee21202ce72
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-10-20 01:15:12 +0200

    Create RoyaltyToken.sol

---
+++ RoyaltyToken.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import { ERC20 } from "https://github.com/transmissions11/solmate/blob/c2594bf4635ad773a8f4763e20b7e79582e41535/src/tokens/ERC20.sol";
+
+contract RoyaltyToken is ERC20 {
+    
+    address public royaltyAddress;
+    uint256 public royaltyFeePercentage;
+
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimals,
+        uint256 _royaltyFeePercentage,
+        uint256 _initialSupply
+    ) ERC20(_name, _symbol, _decimals) {
+        royaltyAddress = msg.sender;
+        royaltyFeePercentage = _royaltyFeePercentage;
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function transferWithRoyalty (address to, uint256 amount) public returns (bool) {
+        uint256 royaltyAmount = amount * royaltyFeePercentage / 100;
+
+        transfer(royaltyAddress, royaltyAmount);
+
+        transfer(to, amount - royaltyAmount);
+
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        uint256 royaltyAmount = amount * royaltyFeePercentage / 100;
+        
+        balanceOf[msg.sender] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount - royaltyAmount;
+            balanceOf[royaltyAddress] += royaltyAmount;
+        }
+        //transfer to the royalty address
+        emit Transfer(msg.sender, royaltyAddress, royaltyAmount);
+        //transfer to the original address
+        emit Transfer(msg.sender, to, amount - royaltyAmount);
+
+        return true;
+    }
+}
